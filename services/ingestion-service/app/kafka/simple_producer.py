f0a337ba220aeec50851a3bb72da8afc01cf2bc2
Projjwal Roy Chowdhury <projjwalroychowdhury@gmail.com>
2026-03-20 01:09:09 +0530

    feat: Update Kafka producer configuration and enhance ingestion metrics endpoint

--- services/ingestion-service/app/kafka/simple_producer.py
+++ services/ingestion-service/app/kafka/simple_producer.py
@@ -1,29 +1,29 @@
-from kafka import KafkaProducer # type: ignore
+from kafka import KafkaProducer #type: ignore
 import json
-from app.logger import logger # type: ignore
 from typing import Dict, Any
 
 producer = None
 
-
-def _init_producer():
+def init_producer():
     global producer
+    producer = KafkaProducer(
+        bootstrap_servers="localhost:29092",
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),  #type: ignore
+        api_version=(0, 10)
+    )
+
+def create_producer() -> KafkaProducer:
+    return KafkaProducer(
+        bootstrap_servers="localhost:29092",
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),#type: ignore
+        api_version=(0, 10)
+    )
+
+def get_producer():
+    return producer
+
+def send_metric(metric: Dict[str, Any]) -> None:
     if producer is None:
-        try:
-            producer = KafkaProducer(
-                bootstrap_servers="localhost:9092",
-                value_serializer=lambda v: json.dumps(v).encode("utf-8") # type: ignore
-            )
-            logger.info("Initialized Kafka producer", extra={"bootstrap": "localhost:9092"}) # type: ignore
-        except Exception as e:
-            logger.error("Unable to initialize Kafka producer", extra={"error": str(e)}) # type: ignore
-            raise
-
-
-def send_metric(metric: Dict[str, Any]):
-    _init_producer()
-    try:
-        producer.send("metrics_raw", metric) # type: ignore
-    except Exception as e:
-        logger.error("Failed to send metric to Kafka", extra={"error": str(e)}) # type: ignore
-        raise
+        init_producer()
+    producer.send("metrics_raw", metric) # type: ignore
+    producer.flush() # type: ignore
