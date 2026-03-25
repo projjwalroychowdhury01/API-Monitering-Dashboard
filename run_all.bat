3a8970f8c071cd1d3c136a7e79c44b32e59039d3
Projjwal Roy Chowdhury <projjwalroychowdhury@gmail.com>
2026-03-26 01:48:16 +0530

    feat: Add ClickHouse 5-minute aggregation with materialized view and implement Python writer for 1-minute metrics.

--- run_all.bat
+++ run_all.bat
@@ -17,7 +17,14 @@ echo ==============================
 echo Ensuring ClickHouse Table
 echo ==============================
 
-docker exec -i %CLICKHOUSE_CONTAINER% clickhouse-client --query "CREATE TABLE IF NOT EXISTS metrics_1m (endpoint_id String, timestamp UInt32, p50 Float32, p95 Float32, p99 Float32, request_count UInt32, error_rate Float32) ENGINE = MergeTree() PARTITION BY toDate(timestamp) ORDER BY (endpoint_id, timestamp);"
+docker exec -i %CLICKHOUSE_CONTAINER% clickhouse-client --query "DROP TABLE IF EXISTS metrics_1m;"
+docker exec -i %CLICKHOUSE_CONTAINER% clickhouse-client --query "CREATE TABLE IF NOT EXISTS metrics_1m (endpoint_id String, timestamp DateTime, p50 Float32, p95 Float32, p99 Float32, request_count UInt32, error_rate Float32) ENGINE = MergeTree() PARTITION BY toDate(timestamp) ORDER BY (endpoint_id, timestamp) TTL timestamp + INTERVAL 30 DAY;"
+
+docker exec -i %CLICKHOUSE_CONTAINER% clickhouse-client --query "DROP TABLE IF EXISTS metrics_5m;"
+docker exec -i %CLICKHOUSE_CONTAINER% clickhouse-client --query "CREATE TABLE IF NOT EXISTS metrics_5m (endpoint_id String, timestamp DateTime, p50 Float32, p95 Float32, p99 Float32, request_count UInt32, error_rate Float32) ENGINE = MergeTree() PARTITION BY toDate(timestamp) ORDER BY (endpoint_id, timestamp) TTL timestamp + INTERVAL 90 DAY;"
+
+docker exec -i %CLICKHOUSE_CONTAINER% clickhouse-client --query "DROP VIEW IF EXISTS metrics_5m_mv;"
+docker exec -i %CLICKHOUSE_CONTAINER% clickhouse-client --query "CREATE MATERIALIZED VIEW metrics_5m_mv TO metrics_5m AS SELECT endpoint_id, timestamp, quantile(0.5)(p50) AS p50, quantile(0.95)(p95) AS p95, quantile(0.99)(p99) AS p99, sum(request_count) AS request_count, avg(error_rate) AS error_rate FROM (SELECT endpoint_id, toStartOfFiveMinute(timestamp) AS timestamp, p50, p95, p99, request_count, error_rate FROM metrics_1m) GROUP BY endpoint_id, timestamp;"
 
 echo ==============================
 echo Starting Ingestion Service
